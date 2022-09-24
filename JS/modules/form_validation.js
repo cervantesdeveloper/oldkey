9e5dbfc94e887a175fe352919ecdae0018009d58
Victor <cervantesdeveloper@gmail.com>
2022-09-23 21:33:44 -0500

    I changed the email

--- JS/modules/form_validation.js
+++ JS/modules/form_validation.js
@@ -35,7 +35,7 @@ export default function formValidation(form, inputs, loader, response){
 
         $loader.classList.remove("hidden");
 
-        fetch("https://formsubmit.co/ajax/ale_vieyra39@hotmail.com", {
+        fetch("https://formsubmit.co/ajax/oldkey.ok@gmail.com", {
             method: "POST",
             body: new FormData(e.target)
         })
